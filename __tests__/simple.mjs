7bbdbc0ab89c6e87d58c206d061f5eef3f3197b3
William Wong <compulim@hotmail.com>
2021-07-18 22:57:52 -0700

    Clean up

--- __tests__/simple.mjs
+++
@@ -1,9 +0,0 @@
-import AbortController from '../lib/esm/index.mjs';
-
-(async function () {
-  const controller = new AbortController();
-
-  controller.signal.addEventListener('abort', () => process.send('aborted'));
-
-  setTimeout(() => controller.abort(), 0);
-})();
