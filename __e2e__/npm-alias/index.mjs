ddd6a1da84e0d4e5ef111a42066f65d3d4fbc089
William Wong <compulim@users.noreply.github.com>
2022-02-28 12:54:07 +0000

    Fix tests

--- __e2e__/npm-alias/index.mjs
+++ __e2e__/npm-alias/index.mjs
@@ -1,4 +1,4 @@
-// "ms" is being aliased as "p-defer", thus, "p-defer-es5" is actually "ms".
-const ms = require('p-defer-es5');
+// "ms" is being aliased as "abort-controller", thus, "abort-controller-es5" is actually "ms".
+const ms = require('abort-controller-es5');
 
 console.log(ms(1000));
--- __e2e__/web.mjs
+++ __e2e__/web.mjs
@@ -1,9 +1,9 @@
 // When bundling with esbuild:
-// - If we "import", it load ESM from "markdown-it-attrs-es5/module". The default imported object is () => any.
-// - If we "require", it load CJS from "markdown-it-attrs-es5/main". The default imported object is () => any.
+// - If we "import", it load ESM from "abort-controller-attrs-es5/module". The default imported object is () => any.
+// - If we "require", it load CJS from "abort-controller-attrs-es5/main". The default imported object is () => any.
 
 // When bundling with Webpack:
-// - Nomatter we "import" or "require", it always load ESM from "markdown-it-attrs-es5/module", based on the "webpack.config.json/mainFields".
+// - Nomatter we "import" or "require", it always load ESM from "abort-controller-attrs-es5/module", based on the "webpack.config.json/mainFields".
 // - If we "import", the default imported object is () => any.
 // - If we "require", the default imported object is { default: () => any }.
 // - If we "require" and "mainFields" is "main", the default imported object is () => any.
